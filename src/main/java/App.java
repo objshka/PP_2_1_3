b27bf0c6180f2a467aa6459150d4520e338153e0
objshka <91363173+objshka@users.noreply.github.com>
2023-05-06 21:23:27 +0300

    first init

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,11 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.equals(bean4));
         System.out.println(bean.getMessage());
+        System.out.println(bean2.getMessage());
     }
 }
